55503d28123d1138c78cd0677451315a57a1d349
nicocerero <nicolas.cerero@opendeusto.es>
2022-01-05 21:17:06 +0100

    Project creation

---
+++ src/main/java/cs/stackexchange/StackExchange/App.java
@@ -0,0 +1,13 @@
+package cs.stackexchange.StackExchange;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cs/stackexchange/StackExchange/AppTest.java
@@ -0,0 +1,20 @@
+package cs.stackexchange.StackExchange;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
